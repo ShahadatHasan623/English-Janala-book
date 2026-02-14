f99472e9cfba1d6f049742d1f67006ae6e61b27e
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-02-14 11:28:39 -0800

    added dynamic button api load

--- script/index.js
+++ script/index.js
@@ -6,6 +6,23 @@ const loadLesson = () => {
             displayLession(json.data)
         })
 }
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then((json) => displayWord(json.data))
+}
+
+const displayWord = (word) => {
+    const wordContainer = document.getElementById('word-container')
+    wordContainer.innerHTML = ''
+    word.forEach(wordL => {
+        const cardDiv = document.createElement('div')
+        cardDiv.innerHTML = `<h1>${wordL.word}</h1>
+        `
+        wordContainer.append(cardDiv)
+    });
+}
 const displayLession = (level) => {
     const lessionContainer = document.getElementById('level-container')
     lessionContainer.innerHTML = ''
@@ -13,7 +30,7 @@ const displayLession = (level) => {
         const divBtn = document.createElement('div')
         divBtn.innerHTML = `
          
-         <button class="btn btn-outline btn-primary list-none">
+         <button onclick=loadLevelWord(${less.level_no}) class="btn btn-outline btn-primary list-none">
              <i class="fa-solid fa-book-open"></i> Lession - ${less.level_no}
          </button>
         `
