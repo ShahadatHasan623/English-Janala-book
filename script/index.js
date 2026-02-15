a6ee83e7710c046e3681a25724f0ae6095b46f8e
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-02-15 02:51:28 -0800

    added manage spinner added functionality

--- script/index.js
+++ script/index.js
@@ -1,3 +1,9 @@
+const createElement = (arr) => {
+    const synonyms = arr.map(synms => `<span class="btn">${synms}</span>`
+    )
+    return synonyms.join(" ")
+}
+
 // button level lession api 
 const loadLesson = () => {
     const url = 'https://openapi.programming-hero.com/api/levels/all' //button level get api
@@ -14,6 +20,7 @@ const removeActive = () => {
     })
 }
 const loadLevelWord = (id) => {
+    manageSpinner(true)
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
         .then(res => res.json())
@@ -26,6 +33,53 @@ const loadLevelWord = (id) => {
 
 }
 
+const loadWordDetails = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`
+    const res = await fetch(url)
+    const details = await res.json()
+    displayWordDetails(details.data)
+
+}
+
+// {
+//     "word": "Cautious",
+//     "meaning": "সতর্ক",
+//     "pronunciation": "কশাস",
+//     "level": 2,
+//     "sentence": "Be cautious while crossing the road.",
+//     "points": 2,
+//     "partsOfSpeech": "adjective",
+//     "synonyms": [
+//         "careful",
+//         "alert",
+//         "watchful"
+//     ],
+//     "id": 3
+// }
+const displayWordDetails = (details) => {
+
+    const wordModal = document.getElementById("modal-container")
+    wordModal.innerHTML = ""
+    wordModal.innerHTML = ` <div class="space-y-2">
+                    <h1 class="text-2xl font-semibold hind-siliguri">${details.word} (<i class="fa-solid fa-microphone-lines"></i>:${details.pronunciation})</h1>
+                    <div>
+                        <h2 class="text-2xl font-semibold">Meaning</h2>
+                        <p class="text-xl hind-siliguri">${details.meaning}</p>
+                        <h3 class="text-2xl font-semibold">Example</h3>
+                        <p class="text-xl text-gray-600">${details.sentence}</p>
+                    </div>
+                    <div>
+                        <h1 class="text-2xl font-semibold hind-siliguri">সমার্থক শব্দ গুলো</h1>
+                        <div class="flex items-center gap-2 text-lg font-medium text-gray-700">
+                            <div>
+                               ${createElement(details.synonyms)}
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+    document.getElementById("word_modal").showModal()
+}
+
 // {
 //     "id": 1,
 //     "level": 3,
@@ -33,6 +87,17 @@ const loadLevelWord = (id) => {
 //     "meaning": null,
 //     "pronunciation": "অবানডান্ট"
 // }
+
+const manageSpinner = (status) => {
+    if (status == true) {
+        document.getElementById('spinner').classList.remove("hidden")
+        document.getElementById('word-container').classList.add("hidden")
+    }
+    else {
+        document.getElementById('word-container').classList.remove("hidden")
+        document.getElementById('spinner').classList.add("hidden")
+    }
+}
 
 const displayWord = (word) => {
 
@@ -47,6 +112,8 @@ const displayWord = (word) => {
             <h1 class="hind-siliguri text-4xl font-bold">নেক্সট Lesson এ যান</h1>
         </div>
         `
+        manageSpinner(false)
+        return
     }
     word.forEach(wordL => {
         const cardDiv = document.createElement('div')
@@ -56,13 +123,14 @@ const displayWord = (word) => {
             <p>${wordL.word} /${wordL.pronunciation}</p>
             <h2 class="text-xl font-semibold hind-siliguri">"${wordL.meaning ? wordL.meaning : "অর্থ পাওয়া যায় নি"} / ${wordL.pronunciation ? wordL.pronunciation : "pronunciation পাওয়া যায় নি"}"</h2>
             <div class="flex justify-between items-center mt-5">
-                <button onclick="my_modal_5.showModal()" class="btn bg-[#37495710]"><i class="fa-solid fa-circle-info fa-lg"></i></button>
+                <button onclick="loadWordDetails(${wordL.id})" class="btn bg-[#37495710]"><i class="fa-solid fa-circle-info fa-lg"></i></button>
                 <button class="btn bg-[#37495710]"><i class="fa-solid fa-volume-high z-5 fa-lg"></i></button>
             </div>
         </div>
         `
         wordContainer.append(cardDiv)
     });
+    manageSpinner(false)
 }
 
 // lession button show 
